58f705d7235e577ee107a064e53480d87bb2ca04
yianxd <jean4520rodriguez@gmail.com>
2024-10-17 09:04:15 -0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-app2';
+  title = 'Directivas y otras cosas';
+  mensaje = "mensaje con el unico fin de seguir el tutorial"
+  registro= false
+  nombre:string=""
+  apellido:string=""
+  entradas:object[];
+
+  constructor(){
+    this.entradas= [
+
+      {title:"un titulo",
+        name: "hola"
+      },
+      {title:"un titulo5"},
+      {title:"un titulo2"},
+      {title:"un titulo3"},
+      {title:"un titulo4"},
+    ]
+  }
+
+
+  registroUsuario(){
+
+    this.registro = true
+    this.mensaje="usuario registrado con exito";
+  }
 }
